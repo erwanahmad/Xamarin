8eb0c9b1c7dfade7a237badfb167a7e985a6728d
Erwan Ahmad Ardiansyah <ardiansyaherwan40@gmail.com>
2017-09-11 14:21:26 +0700

    Add files via upload

---
+++ Xamarin/Xamarin_app/Xamarin_app/Xamarin_app/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_app
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        public void ButtonClicked(object o, EventArgs e)
+        {
+            Title.Text = "Goodby, Benn";
+        }
+	}
+}
